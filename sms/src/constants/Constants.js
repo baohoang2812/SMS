1b874de2ba0a87cbdc3c4e9383d3907b4e5c8bee
Gia Bảo Hoàng <61697588+baohoang2812@users.noreply.github.com>
2020-09-04 00:40:31 +0700

    Change API EndPoint

--- sms/src/constants/Constants.js
+++ sms/src/constants/Constants.js
@@ -1,5 +1,5 @@
 const API = {
-  END_POINT: 'http://192.168.0.104:45455/api/',
+  END_POINT: 'https://studentmanagementprc.azurewebsites.net/api/',
   BASE_URL: 'https://studentmanagementprc.azurewebsites.net/',
 };
 
